bf28854d45cde107336f241bdc1cf63f8f1a220a
Analu Costa <costaanact@gmail.com>
2022-12-08 18:06:43 -0300

    .

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+
+        System.out.printf("Products: %n %s, which price is $%.1f%n %s, which price is $%.1f%n", product1, price1, product2, price2);
+        System.out.printf("Record: %d years old, code %d and gender: %c", age, code, gender);
+        System.out.printf("Measure with eight decimal places: %.8f%n Rouded (three decimal places): %.3f%n", measure, measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f", measure);
+
+    }
+}
